123e4a24ee18d6817b1f29a84899ee67d4d86f80
Ake <akhe@grodansparadis.com>
2021-09-06 20:15:02 +0200

    Updated project files

--- pic18_bootloader/PIC18 Bootloader.X/nbproject/Makefile-local-pic18f26K80.mk
+++ pic18_bootloader/PIC18 Bootloader.X/nbproject/Makefile-local-pic18f26K80.mk
@@ -14,24 +14,24 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-PATH_TO_IDE_BIN=/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/
+SHELL=cmd.exe
+PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/v3.00.02-beta/mplab_ide/mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=C:/Program Files (x86)/Microchip/MPLABX/v3.00.02-beta/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="/opt/microchip/mplabx/v5.45/sys/java/zulu8.40.0.25-ca-fx-jre8.0.222-linux_x64/bin/"
+MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\v3.00.02-beta\sys\java\jre1.7.0_67/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="/opt/microchip/xc8/v1.21/bin/xc8"
+# MP_CC is not defined
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="/opt/microchip/xc8/v1.21/bin/xc8"
-MP_LD="/opt/microchip/xc8/v1.21/bin/xc8"
-MP_AR="/opt/microchip/xc8/v1.21/bin/xc8"
-DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="/opt/microchip/xc8/v1.21/bin"
+MP_AS="C:\Program Files (x86)\Microchip\MPLABX\mpasmx\mpasmx.exe"
+MP_LD="C:\Program Files (x86)\Microchip\MPLABX\mpasmx\mplink.exe"
+MP_AR="C:\Program Files (x86)\Microchip\MPLABX\mpasmx\mplib.exe"
+DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/v3.00.02-beta/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+# MP_CC_DIR is not defined
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="/opt/microchip/xc8/v1.21/bin"
-MP_LD_DIR="/opt/microchip/xc8/v1.21/bin"
-MP_AR_DIR="/opt/microchip/xc8/v1.21/bin"
+MP_AS_DIR="C:\Program Files (x86)\Microchip\MPLABX\mpasmx"
+MP_LD_DIR="C:\Program Files (x86)\Microchip\MPLABX\mpasmx"
+MP_AR_DIR="C:\Program Files (x86)\Microchip\MPLABX\mpasmx"
 # MP_BC_DIR is not defined
-DFP_DIR=/opt/microchip/mplabx/v5.45/packs/Microchip/PIC18Fxxxx_DFP/1.2.26
