c389feda16e03dffcf71f51832192a3c6c6067cf
Ake Hedman <akhe@grodansparadis.com>
2025-02-07 18:02:13 +0100

    Added raw copy for future manual update

---
+++ manual/wiki/manual/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$eaCpIU7N$wOIP1LBgHQnrj4TfLIYdU0:Ake Hedman:akhe@grodansparadis.com:admin,user
